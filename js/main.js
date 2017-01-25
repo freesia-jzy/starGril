cb64ce129b33f16fb02e919f32bcb2946cab3ca0
grace-lalala <1253110928@qq.com>
2017-01-25 16:51:27 +0800

    鼠标控制星星显示与隐藏

--- js/main.js
+++ js/main.js
@@ -11,12 +11,21 @@ var starPic = new Image();
 var num = 60;
 var stars = [];
 
+var lastTime;
+var deltaTime;
+
+var switchy = false;//判断鼠标是否在图片区域，如果在是true，如果不在是false
+var life = 0;
+
 function init() {
     can = document.getElementById("canvas");
     ctx = can.getContext("2d");
 
     w = can.width;
     h = can.height;
+
+    document.addEventListener("mousemove",mousemove,false);
+
     girlPic.src = "src/girl.jpg";
     starPic.src = "src/star.png";
     for(var i = 0;i<num;i++){
@@ -25,6 +34,7 @@ function init() {
         stars.push(obj);
         stars[i].init();
     }
+    lastTime = Date.now();
     gameloop();
 
 
@@ -34,9 +44,14 @@ document.body.onload=init;
 
 function gameloop() {
    window.requestAnimationFrame(gameloop);
+
+    var now = Date.now();
+    deltaTime = now-lastTime;
+    lastTime = now;
     drawBackground();
     drawGirl();
     drawStars();
+    aliveUpdate();
 }
 function drawBackground() {
     ctx.fillStyle = "#393550";
@@ -46,4 +61,17 @@ function drawGirl() {
     //drawImage(img,x,y,width,height)
     //x轴坐标正方向向右，y轴坐标正方向向下，（0,0）在canvas左上角
     ctx.drawImage(girlPic,100,100,600,400);
+}
+function mousemove(e) {
+    if(e.offsetX||e.layerX){
+        var px = e.offsetX ==undefined?e.layerX :e.offsetX;
+        var py = e.offsetY == undefined?e.layerY:e.offsetY;
+
+        if(px>100&&px<700&&py>100&&py<500){
+            switchy = true;
+        }
+        else {
+            switchy = false;
+        }
+    }
 }
--- js/stars.js
+++ js/stars.js
@@ -4,18 +4,78 @@
 var starObj = function () {
     this.x;
     this.y;
+
+    this.picNo;
+    this.timer;
+
+    this.xSpd;
+    this.ySpd;
 };
 starObj.prototype.init = function () {
     this.x = Math.random()*600+100;
     this.y = Math.random()*300+150;
+
+    //this.picNo = 0;
+    this.picNo = Math.floor(Math.random()*7);
+    this.timer = 0;
+
+    this.xSpd = Math.random()*3-1.5;
+    this.ySpd = Math.random()*3-1.5;
+};
+starObj.prototype.update = function () {
+    this.x += this.xSpd*deltaTime*0.004;
+    this.y += this.ySpd*deltaTime*0.004;
+
+    if(this.x<100||this.x>700){
+        this.init();
+        return;
+    }
+    if(this.y<100||this.y>500){
+        this.init();
+        return;
+    }
+
+    this.timer += deltaTime;
+    if(this.timer >50){
+        this.picNo += 1;
+        this.picNo %=7;
+        this.timer = 0;
+    }
+
+
 };
 starObj.prototype.draw = function () {
-    ctx.drawImage(starPic,this.x,this.y);
+    ctx.save();
+
+    ctx.globalAlpha = life;//globalAlpha是全局作用域，对所有画布上元素都有影响，但是
+    //如果加上save()和restore()函数，就只在这两者中间的元素的起作用
+
+    ctx.drawImage(starPic,this.picNo*7,0,7,7,this.x,this.y,7,7);
+
+    ctx.restore();
 };
 
 
 function drawStars() {
     for(var i=0;i<num;i++){
+        stars[i].update();
         stars[i].draw();
+    }
+}
+function aliveUpdate() {
+    if(switchy){
+        //show stars
+        life += 0.03*deltaTime*0.05;
+        if(life >1){
+            life = 1;
+        }
+    }
+    else{
+        //hide stars
+        life -= 0.03*deltaTime*0.05;
+        if(life<0){
+            life = 0;
+        }
+
     }
 }
